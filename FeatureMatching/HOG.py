28f87b878d8ae1a9c68d40e2c6471b85d486cc5f
anhle150199 <anhle150199@gmail.com>
2021-06-30 16:25:06 +0700

    update:

--- FeatureMatching/HOG.py
+++ FeatureMatching/HOG.py
@@ -5,9 +5,10 @@ from skimage import exposure
 import matplotlib.pyplot as plt
 
 # reading the image
-img = imread("../Photos/avatar2.jpg")
+img = imread("../Detect/object-detector-master/data/dataset/pos/pos (92).jpg")
 fd, hog_image = hog(img, orientations=9, pixels_per_cell=(8, 8),
                 	cells_per_block=(2, 2), visualize=True, multichannel=True)
+print(hog_image)
 plt.axis("off")
 plt.imshow(hog_image, cmap="gray")
 plt.show()
